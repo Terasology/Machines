8089deea193745823d45937d4114e80fa96d724b
Josh Zacharias <josh.owie@gmail.com>
2014-11-06 16:24:33 -0800

    Fire an event on requirement use.

--- src/main/java/org/terasology/machines/processParts/RequirementInputComponent.java
+++ src/main/java/org/terasology/machines/processParts/RequirementInputComponent.java
@@ -22,6 +22,7 @@ import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.logic.inventory.InventoryManager;
 import org.terasology.machines.ExtendedInventoryManager;
 import org.terasology.machines.components.ProvidesProcessRequirements;
+import org.terasology.machines.events.RequirementUsedEvent;
 import org.terasology.registry.CoreRegistry;
 import org.terasology.workstation.process.DescribeProcess;
 import org.terasology.workstation.process.ProcessPart;
@@ -64,6 +65,29 @@ public class RequirementInputComponent implements Component, ProcessPart, Descri
 
     @Override
     public void executeStart(EntityRef instigator, EntityRef workstation, EntityRef processEntity) {
+        InventoryManager inventoryManager = CoreRegistry.get(InventoryManager.class);
+
+        List<String> requirementsRequired = Lists.newArrayList(requirements);
+
+        // get the requirements provided by the machine
+        for (Component component : workstation.iterateComponents()) {
+            if (component instanceof ProvidesProcessRequirements) {
+                if (requirementsRequired.removeAll(Lists.newArrayList(((ProvidesProcessRequirements) component).getRequirementsProvided()))) {
+                    workstation.send(new RequirementUsedEvent());
+                }
+            }
+        }
+
+        // get the requirements provided by items (tools)
+        for (EntityRef item : ExtendedInventoryManager.iterateItems(inventoryManager, workstation, "REQUIREMENTS")) {
+            for (Component component : item.iterateComponents()) {
+                if (component instanceof ProvidesProcessRequirements) {
+                    if (requirementsRequired.removeAll(Lists.newArrayList(((ProvidesProcessRequirements) component).getRequirementsProvided()))) {
+                        item.send(new RequirementUsedEvent());
+                    }
+                }
+            }
+        }
 
     }
 
