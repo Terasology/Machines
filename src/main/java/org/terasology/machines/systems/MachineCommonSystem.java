28ff6f75ad59721e3ef878c903664c2ef2ab0c86
jdrueckert <jd.rueckert@googlemail.com>
2020-04-27 23:29:22 +0200

    fix: tools with components added in `MachineCommonSystem` (#29)
    - assembly slots (`InventoryGrid`s) were not shown when placing an assembly table
    - this was due to, amongst other components, the `InventoryComponent` being removed in engine's `EntityAwareWorldProvider::updateBlockEntityComponents` because it is neither a "common component", nor part of the assembly table's prefab
    - by adding a `RetainComponentsComponent` containing the respective components that were added programmatically in `MachineCommonSystem:addProcessingMachine` they are not removed anymore
    
    - add .gitattributes

--- src/main/java/org/terasology/machines/systems/MachineCommonSystem.java
+++ src/main/java/org/terasology/machines/systems/MachineCommonSystem.java
@@ -15,12 +15,14 @@
  */
 package org.terasology.machines.systems;
 
+import org.terasology.entitySystem.entity.EntityBuilder;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.entity.lifecycleEvents.OnAddedComponent;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.fluid.component.FluidInventoryComponent;
+import org.terasology.logic.common.RetainComponentsComponent;
 import org.terasology.logic.inventory.InventoryAccessComponent;
 import org.terasology.logic.inventory.InventoryComponent;
 import org.terasology.machines.components.MachineDefinitionComponent;
@@ -49,9 +51,11 @@ public class MachineCommonSystem extends BaseComponentSystem {
     }
 
     private void addProcessingMachine(EntityRef entity, MachineDefinitionComponent machineDefinition) {
+        RetainComponentsComponent retainComponents = new RetainComponentsComponent();
 
         // configure the input/output inventories
         if (!entity.hasComponent(InventoryComponent.class) && machineDefinition.inputSlots + machineDefinition.requirementSlots + machineDefinition.outputSlots > 0) {
+            retainComponents.components.add(InventoryComponent.class);
             int totalSlots = machineDefinition.inputSlots + machineDefinition.requirementSlots + machineDefinition.outputSlots;
             InventoryComponent inventoryComponent = new InventoryComponent(totalSlots);
             inventoryComponent.privateToOwner = false;
@@ -60,6 +64,7 @@ public class MachineCommonSystem extends BaseComponentSystem {
 
         // configure the fluid inventories
         if (!entity.hasComponent(FluidInventoryComponent.class) && machineDefinition.fluidInputSlotVolumes.size() + machineDefinition.fluidOutputSlotVolumes.size() > 0) {
+            retainComponents.components.add(FluidInventoryComponent.class);
             FluidInventoryComponent fluidInventoryComponent = new FluidInventoryComponent();
             for (float volume : machineDefinition.fluidInputSlotVolumes) {
                 fluidInventoryComponent.fluidSlots.add(EntityRef.NULL);
@@ -74,6 +79,7 @@ public class MachineCommonSystem extends BaseComponentSystem {
 
         // configure the categorized inventory
         if (!entity.hasComponent(InventoryAccessComponent.class) && (entity.hasComponent(InventoryComponent.class) || entity.hasComponent(FluidInventoryComponent.class))) {
+            retainComponents.components.add(InventoryAccessComponent.class);
             InventoryAccessComponent categorizedInventory = new InventoryAccessComponent();
             categorizedInventory.input = new HashMap();
             categorizedInventory.output = new HashMap();
@@ -101,6 +107,7 @@ public class MachineCommonSystem extends BaseComponentSystem {
 
             entity.addComponent(categorizedInventory);
         }
+        entity.addOrSaveComponent(retainComponents);
     }
 
     IntegerRange createSlotRange(int startIndex, int length) {
