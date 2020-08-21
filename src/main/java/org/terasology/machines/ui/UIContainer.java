16b942dbf255e2fc209c62a21f05ef77931748a7
DarkWeird <foxflameless@gmail.com>
2020-08-21 17:51:44 +0300

    fix: NUI extraction compability

--- src/main/java/org/terasology/machines/ui/UIContainer.java
+++ src/main/java/org/terasology/machines/ui/UIContainer.java
@@ -1,26 +1,13 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.machines.ui;
 
 
-import org.terasology.math.geom.Vector2i;
-import org.terasology.rendering.nui.Canvas;
-import org.terasology.rendering.nui.CoreWidget;
-import org.terasology.rendering.nui.LayoutConfig;
-import org.terasology.rendering.nui.UIWidget;
+import org.joml.Vector2i;
+import org.terasology.nui.Canvas;
+import org.terasology.nui.CoreWidget;
+import org.terasology.nui.LayoutConfig;
+import org.terasology.nui.UIWidget;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -43,7 +30,7 @@ public class UIContainer extends CoreWidget {
         if (content != null) {
             return canvas.calculateRestrictedSize(content, sizeHint);
         }
-        return Vector2i.zero();
+        return new Vector2i();
     }
 
     public UIWidget getContent() {
--- src/main/java/org/terasology/mechanicalPower/systems/MechanicalPowerClientSystem.java
+++ src/main/java/org/terasology/mechanicalPower/systems/MechanicalPowerClientSystem.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.mechanicalPower.systems;
 
 import org.terasology.engine.Time;
@@ -40,7 +27,7 @@ import org.terasology.mechanicalPower.components.RotatingAxleComponent;
 import org.terasology.potentialEnergyDevices.components.PotentialEnergyDeviceComponent;
 import org.terasology.registry.In;
 import org.terasology.rendering.nui.layers.ingame.inventory.GetItemTooltip;
-import org.terasology.rendering.nui.widgets.TooltipLine;
+import org.terasology.nui.widgets.TooltipLine;
 import org.terasology.world.block.BlockComponent;
 import org.terasology.world.block.BlockManager;
 import org.terasology.world.block.items.BlockItemComponent;
