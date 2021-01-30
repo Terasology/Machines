f356223b1e5d251242c474d68a2a4967a23acb1d
Tobias Nett <skaldarnar@googlemail.com>
2021-01-30 22:31:59 +0100

    feat(JOML): remove deprecated API in favor of JOML for BlockFamily (#47)

--- src/main/java/org/terasology/machines/world/SurfacePlacementFamily.java
+++ src/main/java/org/terasology/machines/world/SurfacePlacementFamily.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.machines.world;
 
 import com.google.common.collect.Maps;
@@ -20,7 +7,6 @@ import org.terasology.math.Pitch;
 import org.terasology.math.Rotation;
 import org.terasology.math.Side;
 import org.terasology.math.Yaw;
-import org.terasology.math.geom.Vector3i;
 import org.terasology.naming.Name;
 import org.terasology.world.block.Block;
 import org.terasology.world.block.BlockBuilderHelper;
@@ -79,11 +65,6 @@ public class SurfacePlacementFamily extends AbstractBlockFamily implements SideD
     @Override
     public Block getBlockForPlacement(BlockPlacementData data) {
         return blocks.get(data.attachmentSide);
-    }
-
-    @Override
-    public Block getBlockForPlacement(Vector3i location, Side attachmentSide, Side direction) {
-        return blocks.get(attachmentSide);
     }
 
     @Override
