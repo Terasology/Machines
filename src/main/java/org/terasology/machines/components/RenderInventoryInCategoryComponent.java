755553c03f9a3c2ed1bfce58a9317fcf20cd682f
DarkWeird <foxflameless@gmail.com>
2021-07-09 16:53:18 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components.

--- src/main/java/org/terasology/machines/components/RenderInventoryInCategoryComponent.java
+++ src/main/java/org/terasology/machines/components/RenderInventoryInCategoryComponent.java
@@ -1,28 +1,21 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.machines.components;
 
-import org.terasology.engine.entitySystem.Component;
+import org.terasology.gestalt.entitysystem.component.Component;
 import org.terasology.itemRendering.systems.RenderOwnedEntityDetails;
 
 /**
  * Add this to a block that you want items displayed from an inventory category.
  * Also add the RenderItemComponent to adjust the location of the item,  otherwise it will be in the center of the containing block.
  */
-public class RenderInventoryInCategoryComponent extends RenderOwnedEntityDetails implements Component {
+public class RenderInventoryInCategoryComponent extends RenderOwnedEntityDetails implements Component<RenderInventoryInCategoryComponent> {
     public String category;
     public boolean isOutputCategory;
+
+    @Override
+    public void copy(RenderInventoryInCategoryComponent other) {
+        this.category = other.category;
+        this.isOutputCategory = other.isOutputCategory;
+    }
 }
--- src/main/java/org/terasology/mechanicalPower/components/MechanicalPowerBlockNetworkComponent.java
+++ src/main/java/org/terasology/mechanicalPower/components/MechanicalPowerBlockNetworkComponent.java
@@ -1,27 +1,14 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.mechanicalPower.components;
 
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.world.block.ForceBlockActive;
 import org.terasology.entityNetwork.components.BlockLocationNetworkNodeComponent;
+import org.terasology.gestalt.entitysystem.component.Component;
 import org.terasology.mechanicalPower.systems.MechanicalPowerAuthoritySystem;
 
 @ForceBlockActive
-public class MechanicalPowerBlockNetworkComponent extends BlockLocationNetworkNodeComponent implements Component {
+public class MechanicalPowerBlockNetworkComponent extends BlockLocationNetworkNodeComponent implements Component<MechanicalPowerBlockNetworkComponent extends BlockLocationNetworkNodeComponent> {
     public MechanicalPowerBlockNetworkComponent() {
         networkId = MechanicalPowerAuthoritySystem.NETWORK_ID;
     }
