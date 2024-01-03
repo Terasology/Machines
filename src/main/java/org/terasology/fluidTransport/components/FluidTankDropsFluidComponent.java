c5fb7ef76fdc795d71e38dfcf2b07b098b9be832
Zangus <wong.thi@gmail.com>
2024-01-03 19:23:42 +0100

    refactor: Components without attributes to EmptyComponent (#57)
    Co-authored-by: thierrywong <wong.t@sfeir.com>

--- src/main/java/org/terasology/fluidTransport/components/FluidTankDropsFluidComponent.java
+++ src/main/java/org/terasology/fluidTransport/components/FluidTankDropsFluidComponent.java
@@ -2,11 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.fluidTransport.components;
 
-import org.terasology.gestalt.entitysystem.component.Component;
+import org.terasology.gestalt.entitysystem.component.EmptyComponent;
 
-public class FluidTankDropsFluidComponent implements Component<FluidTankDropsFluidComponent> {
-    @Override
-    public void copyFrom(FluidTankDropsFluidComponent other) {
-
-    }
+public class FluidTankDropsFluidComponent extends EmptyComponent<FluidTankDropsFluidComponent> {
 }
